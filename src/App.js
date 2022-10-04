9db8f436269a4fa11aafa06709c186938ca82d6e
konmaru_fox <mayuko.kondo.mutwiri@gmail.com>
2022-10-05 00:47:40 +0300

    Changed footer.

--- src/App.js
+++ src/App.js
@@ -133,21 +133,17 @@ const App = () => {
                 </div>
                 <div className="footer-container">
                     <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-                    <a
-                        className="footer-text"
-                        href={TWITTER_LINK}
-                        target="_blank"
-                        rel="noreferrer"
-                    >{`built by @${TWITTER_HANDLE}`}</a>
-                    <a
-                        className="footer-text"
-                    > </a>
-                    <a
-                        className="footer-text"
-                        href="https://twitter.com/_buildspace"
-                        target="_blank"
-                        rel="noreferrer"
-                    >with @_buildspace community</a>
+                    <p className="footer-text">
+                        built <a  
+                            href={TWITTER_LINK}
+                            target="_blank"
+                            rel="noreferrer"
+                        >{`by @${TWITTER_HANDLE}`}</a>, with <a
+                            href="https://twitter.com/_buildspace"
+                            target="_blank"
+                            rel="noreferrer"
+                        >@_buildspace community</a>
+                    </p>             
                 </div>
             </div>
         </div>
